5070ed42d0a377bdbc17e822acbd33743f9058f6
saviocunha <saviocunha.001@hotmail.com>
2023-10-23 13:55:24 -0300

    alteração atividade 03

--- atividade03.py
+++ atividade03.py
@@ -1,9 +1,8 @@
 # dias da semana
-
+print("Domingo")
 print("Segunda-feira")
 print("Terça-feira")
 print("Quarta-feira")
 print("Quinta-feira")
 print("Sexta-feira")
 print("Sábado")
-print("Domingo")
