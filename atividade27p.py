bafe83c5baec256089a975de51501eecc1efcccd
saviocunhaa <saviocunha.01@hotmail.com>
2023-10-31 11:16:23 -0300

    ajustes de logica

--- atividade27p.py
+++ atividade27p.py
@@ -1,5 +1,8 @@
+#pegar a frase.
 frase = str(input('Digite um nome')).upper().replace(" ","")
+#inverter.
 invertido=frase[::-1]
+#verificar se a frase é um palindromo
 if frase == invertido:
     print("palíndromo")
 else:
