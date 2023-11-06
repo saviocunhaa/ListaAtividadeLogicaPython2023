57310510fb80bd0423d5e21837fe1e709d799e2a
saviocunha <saviocunha.001@hotmail.com>
2023-11-06 15:49:49 -0300

    revisão

---
+++ revisao.py
@@ -0,0 +1,113 @@
+# # a = 10 #inteiro
+# # numero = 10.8 #float
+# # nomeUsuario = "Sávio Cunha de Sousa" # STRING
+
+# # print(numero)
+
+# # # 1 + 1
+# # # 5 - 4
+# # # 6*3
+# # # 8/2
+
+# # soma = 5 + 3
+# # soma2 = a + numero
+# print(soma2)
+
+
+# tabela verdade
+
+# # c = 5 > 10 or 6 > 1
+# # print(c)
+
+
+# # calculo de media de aluno
+# # recebo as 3 notas
+# nota1 = float(input("digite a 1ª nota"))
+# nota2 = float(input("digite a 2ª nota"))
+# nota3 = float(input("digite a 3ª nota"))
+# # somar as 3 notas e dividir por 3 que será a media
+# media = (nota1+nota2+nota3)/3
+# # mostro o resultado
+# print(f"Sua média é: {media}")
+# # verificar se a media é aprovada ou reprovada
+# # if media >= 6:
+# #     print(f"Aprovado com média {media}")
+# # else:
+# #     print(f"Reprovado com média {media}")
+
+
+
+# # media for menor que 5 -> reprovado
+# # media for entre 5 e 6 -> recuperação
+# # media for maior que 6 -> aprovado direto
+# if media < 5:
+#     print("reprovado")
+# elif media < 6:
+#     print("recuperação")
+# else:
+#     print("aprovado")
+
+
+
+
+# ESTRUTURA DE REPETIÇÃO
+
+# person = "Sávio"
+
+# for i in range(1,51):
+#     print(f"machadada {i}")
+
+# print(f"Parabéns {person}, a arvore caiu!")
+
+
+
+
+# quero apenas os numeros pares entre 0 - 200
+# soma = 0
+# for i in range(0, 201):
+#     if i % 2 == 0:
+#         print(f" o numero {i} é par")
+        
+
+# i = 0
+# while i < 200:
+#     if i % 2 != 0:
+#         print(f"numero {i} é impar")
+#     i += i # i = i + 1
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
